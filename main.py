24eff53e7ef35671ec5dd9a9db721d54c8c59e0f
Atish03 <atishpersonal003@gmail.com>
2022-03-17 08:22:51 +0530

    Encrypt a directory

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 #nocode
+from encrypt_file import edFile
+import os, time
+
+time.clock = time.time
+
+ed = edFile("this_is_my_very_secure_key")
+
+def encrypt_folder(folder):
+	contents = os.listdir(folder)
+	for c in contents:
+		f = os.path.join(folder, c)
+		if os.path.isfile(f):
+			e = ed.encrypt(f)
+			open(f, "wb").write(bytes(e, encoding = "utf-8"))
+		else:
+			encrypt_folder(f)
+
+def decrypt_folder(folder):
+	contents = os.listdir(folder)
+	for c in contents:
+		f = os.path.join(folder, c)
+		if os.path.isfile(f):
+			e = open(f, "rb").read().decode()
+			try:
+				d = ed.decrypt(e)
+				open(f, "wb").write(d)
+			except:
+				pass
+		else:
+			decrypt_folder(f)
